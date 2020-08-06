80aa279bd505dedb5b037f6a4441bde51ed74455
secretjuju <tmdqh04033@gmail.com>
2020-08-06 15:17:57 +0900

    user 데이터베이스 구조 완성

---
+++ back-end/models/user.js
@@ -0,0 +1,39 @@
+const { sequelize } = require(".");
+
+// user 데이터베이스
+
+module.exports = (sequelize,DataTypes) => {
+    return sequelize.define('user',{
+        idx:{ // 고유번호
+            type : DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey : true
+        },
+        account : { // 계정
+            type: DataTypes.STRING(50),
+            primaryKey : true,
+        },
+        passwd : { // 비밀번호
+            type :DataTypes.STRING(50),
+            allowNull : false
+        },
+        profile_image : { // 프로필사진이 저장된 경로
+            type : DataTypes.STRING(48),
+            unique : true
+        },
+        email : { // 이메일 
+            type : DataTypes.STRING(50),
+            unique : true
+        },name : { // 실제 이름
+            type : DataTypes.STRING(30),
+            allowNull : false
+        },
+        nickname : { // 닉네임
+            type : DataTypes.STRING(20),
+            unique : true
+        },
+        description : { // 간단한 자기소개
+            type : DataTypes.STRING(300) 
+        }
+    })
+}
--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,13 @@ var express = require("express")
 var app = express()
 const PORT = 3000
 
+app.use(express.static('public'));
+
+
+app.get("/",(req,res)=> {
+    res.send("test response")
+})
+
 app.listen(PORT,()=>{
     console.log("server on")
 })
