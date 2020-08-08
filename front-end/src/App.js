26b25834ea00418a2694c4b55dbb2205202116da
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 23:38:08 +0900

    [UPD] MainPage Route path 및 component 설정.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Main from './pages/Main';
+import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Main} />
+                <Route exact path="/" component={MainPage} />
                 <Route exact path="/login" component={LoginPage} />
                 <Route exact path="/register" component={RegisterPage} />
             </Switch>
