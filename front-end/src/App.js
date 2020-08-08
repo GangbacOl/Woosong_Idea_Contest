373835bf565efa0de07254bf5ac3598afc246ca2
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 15:16:45 +0900

    [UPD] Home, SignIn, SignUp Route 설정.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Home from './pages/Home';
+import SignIn from './pages//SignIn';
+import SignUp from './pages//SignUp';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/signin" component={SignIn} />
+                <Route exact path="/signup" component={SignUp} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 export default App;
