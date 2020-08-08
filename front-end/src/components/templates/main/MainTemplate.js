afaaf7fdedf5631dadd721bd104040c6efc68e85
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 23:34:45 +0900

    [ADD] Main Template 생성.

---
+++ front-end/src/components/templates/main/MainTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderOrganism from '../../organisms/main/HeaderOrganism';
+
+const MainTemplate = () => {
+    return (
+        <div className="MainTemplate">
+            <HeaderOrganism />
+        </div>
+    );
+};
+
+export default MainTemplate;
