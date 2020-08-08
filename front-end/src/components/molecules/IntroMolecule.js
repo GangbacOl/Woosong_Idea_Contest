9885ec888da8402c9ed8659ca8876a160903ad54
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 19:27:20 +0900

    [ADD] intro(소개 글귀) Molecule 생성.

---
+++ front-end/src/components/molecules/IntroMolecule.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading2Atom from '../atoms/Heading2Atom';
+import ParagraphAtom from '../atoms/ParagraphAtom';
+
+const IntroMolecule = ({ h2Content, pContent }) => {
+    return (
+        <div className="IntroMolecule">
+            <Heading2Atom h2Content={h2Content} />
+            <ParagraphAtom pContent={pContent} />
+        </div>
+    );
+};
+
+export default IntroMolecule;
