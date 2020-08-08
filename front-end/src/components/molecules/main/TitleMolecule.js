e232a9538b771b1b68d4b8ee51deda8d706d6784
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 23:28:33 +0900

    [ADD] Title Molecule 생성.

---
+++ front-end/src/components/molecules/main/TitleMolecule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Heading2Atom from '../../atoms/Heading2Atom';
+
+const TitleMoleCule = () => {
+    return (
+        <div className="TitleMoleCule">
+            <Heading2Atom h2Content="Reader" />
+        </div>
+    );
+};
+
+export default TitleMoleCule;
