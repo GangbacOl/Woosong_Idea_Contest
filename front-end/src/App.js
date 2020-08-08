c6a04eba9e27ebcb7f708e709845b83050108ea5
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 21:08:33 +0900

    [UPD] LoginPage, RegisterPage, Home import path 수정.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Home from './pages/Home';
-import LoginPage from './pages//LoginPage';
-import RegisterPage from './pages//RegisterPage';
+import Main from './pages/Main';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={Main} />
                 <Route exact path="/login" component={LoginPage} />
                 <Route exact path="/register" component={RegisterPage} />
             </Switch>
