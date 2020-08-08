7cb290c4cfba2e50ef8f85d3f39e0fe3ad1264f3
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 23:36:46 +0900

    [ADD] 1. Main Page 생성. 2. MainTemplate 컴포넌트 추가.

---
+++ front-end/src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainTemplate from '../components/templates/main/MainTemplate';
+
+const MainPage = () => {
+    return (
+        <div className="MainPage">
+            <MainTemplate />
+        </div>
+    );
+};
+
+export default MainPage;
