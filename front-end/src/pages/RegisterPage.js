1a9cadef4858b735b0d0e7113b215e14947e834c
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 19:33:53 +0900

    [ADD] register Page 생성.

---
+++ front-end/src/pages/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegisterTemplate from '../components/templates/RegisterTemplate';
+
+const RegisterPage = () => {
+    return (
+        <div className="RegisterPage">
+            <RegisterTemplate />
+        </div>
+    );
+};
+
+export default RegisterPage;
