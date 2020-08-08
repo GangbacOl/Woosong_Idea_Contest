5bf026c7769635a2a26743533ed9071daebd2898
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 23:34:01 +0900

    [ADD] Header Organism 생성.

---
+++ front-end/src/components/organisms/main/HeaderOrganism.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TitleMolecule from '../../molecules/main/TitleMolecule';
+import NavMolecule from '../../molecules/main/NavMolecule';
+
+const HeaderOrganism = () => {
+    return (
+        <div className="HeaderOrganism">
+            <TitleMolecule />
+            <NavMolecule />
+        </div>
+    );
+};
+
+export default HeaderOrganism;
