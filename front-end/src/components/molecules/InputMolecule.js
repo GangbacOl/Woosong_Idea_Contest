d431b0ca3f1504773836a16b5ac0306ecb68e7b4
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 19:28:11 +0900

    [ADD] input Molecule 생성.

---
+++ front-end/src/components/molecules/InputMolecule.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LabelAtom from '../atoms/LabelAtom';
+import InputAtom from '../atoms/InputAtom';
+
+const InputMolecule = ({ htmlFor, LabelContent, type, value }) => {
+    return (
+        <div className="InputMolecule">
+            <LabelAtom htmlFor={htmlFor} LabelContent={LabelContent} />
+            <InputAtom type={type} value={value} />
+        </div>
+    );
+};
+
+export default InputMolecule;
