6503c7c22461ef6207792ff234302d6afa6261d3
GangbacOl <gangyeonjo336@gmail.com>
2020-08-08 19:33:27 +0900

    [ADD] login Page 생성.

---
+++ front-end/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginTemplate from '../components/templates/LoginTemplate';
+
+const LoginPage = () => {
+    return (
+        <div className="LoginPage">
+            <LoginTemplate />
+        </div>
+    );
+};
+
+export default LoginPage;
