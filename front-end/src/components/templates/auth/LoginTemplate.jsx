0a57dbb875d29da2ffd766dd7950185ee1cd3c5a
Gangbac Ol <gangbacol@Gangbacui-MacBookPro.local>
2020-08-10 06:35:42 -0700

    [UPD] 파일 확장자 변경. js -> jsx

--- front-end/src/components/templates/auth/LoginTemplate.jsx
+++ front-end/src/components/templates/auth/LoginTemplate.jsx
@@ -7,10 +7,7 @@ import LinkAtom from '../../atoms/LinkAtom';
 const LoginTemplate = () => {
     return (
         <div className="LoginTemplate">
-            <IntroMolecule
-                h2Content="Reader에 오신 것을 환영합니다!"
-                pContent="서비스를 사용하시려면 로그인해주세요."
-            />
+            <IntroMolecule h2Content="Reader에 오신 것을 환영합니다!" pContent="서비스를 사용하시려면 로그인해주세요." />
             <LoginForm />
             <InputAtom type="button" value="Login" />
             <LinkAtom path="/register" LinkContent="Reader가 처음이신가요?" />
