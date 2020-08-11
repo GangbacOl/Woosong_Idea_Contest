d1f9ece103d669714f69f978ac3334e90ba42c0f
Gangbac Ol <gangbacol@Gangbacui-MacBookPro.local>
2020-08-11 02:19:58 -0700

    [UPD] SideBar, FeedArea Organism 추가.

--- front-end/src/components/templates/main/MainTemplate.jsx
+++ front-end/src/components/templates/main/MainTemplate.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import HeaderOrganism from '../../organisms/main/HeaderOrganism';
+import SideBarOrganism from '../../organisms/main/SideBarOrganism';
+import FeedAreaOrganism from '../../organisms/main/FeedAreaOrganism';
 
-const MainTemplate = () => {
+const MainTemplate = ({ userThumbnail, userName, pContent, images }) => {
     return (
         <div className="MainTemplate">
             <HeaderOrganism />
+            <SideBarOrganism userThumbnail={userThumbnail} userName={userName} />
+            <FeedAreaOrganism userThumbnail={userThumbnail} userName={userName} pContent={pContent} images={images} />
         </div>
     );
 };
